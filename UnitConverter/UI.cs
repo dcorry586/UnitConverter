8cebf9ba48405c2e766de6e4e64ecb92f6c0aff0
dcorry586 <dylancorry15@gmail.com>
2021-01-16 22:49:33 +0000

    Object Oriented implementation - Phase 1

---
+++ UnitConverter/UI.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * UI.cs
+ * 
+ * Version information
+ * 
+ * Author: Dylan Corry
+ * Date: 02/01/2021 
+ * Description: Class for the console applciations's user interface.
+ * 
+ * Copyright notice
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp
+{ // namespace start
+    class UI
+    { // class UI start
+
+        private string fromChoice;
+        private double fromValue = 00.00;
+        private string toChoice;
+        public void Greeting()
+        {
+            // from
+            Console.WriteLine("Welcome to the console metric to imperial unit  converter.");
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter the desired unit by typing the characters representing that unit inside the ( ).\n" + "For example : (mm). You type 'mm '.\n");
+            Console.WriteLine("Milimetres (mm) \n" + "Centimetres (cm) \n" + "Metres (m) \n" + "Kilograms (kg) \n" + "Celsius (c) \n" + "Mililitres (ml)\n" + "Litres (l)\n" + "Kilometres (km)\n"); // from units
+            fromChoice = Console.ReadLine();
+            Console.WriteLine("From : " + fromChoice);
+
+            Console.WriteLine("Enter numerical value: ");
+            fromValue = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Value = " + fromValue);
+
+            // to
+            Console.WriteLine("Imperial Units To Convert Metric To : \n");
+            Console.WriteLine("Enter the desired unit by typing the characters representing that unit inside the ( ).\n" + "For example : (ft). You type ' ft '.\n");
+            Console.WriteLine("Inch (i) \n" + "Feet (ft) \n" + "Yard (y) \n" + "Pound (lbs) \n" + "Fahrenheit (f) \n" + "Ounces (oz)\n" + "Pint (pts)\n" + "Gallons (gal) \n" + "Miles (mile) \n"); // from units
+            toChoice = Console.ReadLine();
+            Console.WriteLine("To : " + toChoice);
+        }
+        // string fromChoice - filed
+        // public string userFromChoice - property
+        public string userFromChoice // so this property can be accessed inside Units class!
+        {
+            get { return fromChoice; }
+            set { fromChoice = value; }
+        }
+
+        public double userFromValue
+        {
+            get { return fromValue; }
+            set { fromValue = value; }
+        }
+
+        // toChoice
+        public string userToChoice
+        {
+            get { return toChoice; }
+            set { toChoice = value; }
+        }
+
+
+    } // class UI end
+} // namespace end
---
+++ UnitConverter/Units.cs
@@ -0,0 +1,122 @@
+﻿/*
+ * Units.cs
+ * 
+ * Version information
+ * 
+ * Author: Dylan Corry
+ * Date: 02/01/2021 
+ * Description: Class for the the conversion of units (Metric to Imperial).
+ * 
+ * Copyright notice
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp
+{
+    class Units
+    {
+        private double Result = 00.00;
+
+        public void convertMetricToImperial(string fromChoice, string toChoice, double fromValue) // in Program.cs (THE DRIVER) insert the properties inplace of the paramters for the this function
+        { // start of method
+            if (fromChoice == "mm" && toChoice == "i")
+            {
+                Result = fromValue * 0.0393701;
+            }
+            if (fromChoice == "mm" && toChoice == "ft")
+            {
+                Result = fromValue * 0.00328084;
+            }
+            if (fromChoice == "mm" && toChoice == "y")
+            {
+                Result = fromValue * 0.00109361;
+            }
+            if (fromChoice == "cm" && toChoice == "i")
+            {
+                Result = fromValue * 0.393701;
+            }
+            if (fromChoice == "cm" && toChoice == "ft")
+            {
+                Result = fromValue * 0.0328084;
+            }
+            if (fromChoice == "cm" && toChoice == "y")
+            {
+                Result = fromValue * 0.0109361;
+            }
+            if (fromChoice == "m" && toChoice == "i")
+            {
+                Result = fromValue * 39.3701;
+            }
+            if (fromChoice == "m" && toChoice == "ft")
+            {
+                Result = fromValue * 3.280841666667;
+            }
+            if (fromChoice == "m" && toChoice == "y")
+            {
+                Result = fromValue * 1.09361;
+            }
+            if (fromChoice == "m" && toChoice == "mile")
+            {
+                Result = fromValue * 0.000621371;
+            }
+            if (fromChoice == "km" && toChoice == "mile")
+            {
+                Result = fromValue * 0.621371;
+            }
+            if (fromChoice == "kg" && toChoice == "lbs")
+            {
+                Result = fromValue * 2.20462;
+            }
+            if (fromChoice == "kg" && toChoice == "oz")
+            {
+                Result = fromValue * 35.274;
+            }
+            if (fromChoice == "kg" && toChoice == "pts")
+            {
+                Result = fromValue * 0.473176475;
+            }
+            if (fromChoice == "kg" && toChoice == "gal")
+            {
+                Result = fromValue * 0.264172053;
+            }
+            if (fromChoice == "c" && toChoice == "f")
+            {
+                Result = (fromValue * 9 / 5) + 32; // (0°C × 9/5) + 32 = 32°F
+            }
+            if (fromChoice == "ml" && toChoice == "oz")
+            {
+                Result = fromValue * 0.0351951;
+            }
+            if (fromChoice == "ml" && toChoice == "pts")
+            {
+                Result = fromValue * 0.00175975;
+            }
+            if (fromChoice == "ml" && toChoice == "gal")
+            {
+                Result = fromValue * 0.000219969;
+            }
+            if (fromChoice == "l" && toChoice == "oz")
+            {
+                Result = fromValue * 35.1951;
+            }
+            if (fromChoice == "l" && toChoice == "pts")
+            {
+                Result = fromValue * 1.75975;
+            }
+            if (fromChoice == "l" && toChoice == "gal")
+            {
+                Result = fromValue * 0.264172;
+            }
+        } // end of method
+
+        public void PrintResult()
+        {
+            Console.WriteLine(Result.ToString("N2"));
+        }
+    }
+}
